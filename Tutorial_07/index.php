a544cc5d3247402b003417dedb4af2ad45be6433
Nge_Sue <sueyanshin@gmail.com>
2023-04-05 23:07:12 +0630

    preview

--- Tutorial_07/index.php
+++ Tutorial_07/index.php
@@ -27,7 +27,7 @@ session_start();
                         <label for="qrName" class="form-label">QR Name</label>
                         <input type="text" class="form-control  <?php echo (!empty($_SESSION['qrNameError'])) ? 'is-invalid' : ''; ?>" id="qrName" aria-describedby="validationError" name="qrName" value="<?php echo !empty($_SESSION['qrName']) ? $_SESSION['qrName'] : ""; ?>" required placeholder="Enter QR Name">
                         <span id="validationError" class="invalid-feedback">
-                            <?php echo $_SESSION['qrNameError']; ?>
+                            <?php echo (!empty($_SESSION['qrNameError'])) ? $_SESSION['qrNameError'] : ''; ?>
                         </span>
                     </div>
                     <button type="submit" name="generate" class="btn btn-primary mt-4 w-100">Generate</button>
@@ -38,7 +38,7 @@ session_start();
 
     <div class="col-sm-2 text-center m-auto mb-4">
         <div class="card text-center <?php echo empty($_SESSION['generatedImg']) ? 'collapse' : '' ?> ">
-            <img src="images/<?php echo $_SESSION['generatedImg'] ?>   " alt="" style="width:200px;">
+            <img src="images/<?php echo (!empty($_SESSION['generatedImg'])) ? $_SESSION['generatedImg'] : '' ?>   " alt="" style="width:200px;">
         </div>
     </div>
 
